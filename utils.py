ca962b1f9fed18d8d8c430dbfd0bfaae2c39b39c
wangqin123456 <113087446+wangqin123456@users.noreply.github.com>
2024-09-05 11:42:19 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,15 +1,11 @@
-from langchain_openai import ChatOpenAI
 from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory
+from langchain_openai import ChatOpenAI
 
-def get_chat_response(prompt,memory,openai_api_key):
-    model = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=openai_api_key, openai_api_base="https://api.aigc369.com/v1")
+
+
+def get_chat_response(prompt, memory, openai_api_key):
+    model = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=openai_api_key)
     chain = ConversationChain(llm=model, memory=memory)
 
-    response = chain.invoke(
-        {"input": prompt}
-    )
+    response = chain.invoke({"input": prompt})
     return response["response"]
-
-# memory = ConversationBufferMemory(return_messages=True)
-
